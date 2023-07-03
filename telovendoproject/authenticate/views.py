06e8d6917f01d5776e0b40e3391ca98aeee644d6
Álvaro Manterola <alvaro.manterola@gmail.com>
2023-07-03 03:52:34 -0400

    Commit Inicial ABPro3

--- telovendoproject/authenticate/views.py
+++ telovendoproject/authenticate/views.py
@@ -6,7 +6,7 @@ from django.views import View
 from django.utils.decorators import method_decorator
 from django.core.mail import send_mail
 from django.conf import settings
-from .forms import FormularioLogin
+from .forms import FormularioLogin, SignUpForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -39,38 +39,35 @@ class CerrarSesion(View):
         logout(request)
         return redirect('index')
 
-# REQUISITO EJERCICIO GRUPAL 3
-#
-#class Registro(View):
-#    def get(self, request):
-#        formulario = SignUpForm(request.POST)
-#        context = {'formulario': formulario}
-#        template_name = "registration/signup.html"
-#        return render(request, template_name, context)
-#
-#    def post(self, request):
-#        formulario = SignUpForm(request.POST)
-#        if formulario.is_valid():
-#            random_pass = User.objects.make_random_password(
-#                length=8, allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
-#            user = User.objects.create_user(
-#                username=formulario.cleaned_data['username'],
-#                email=formulario.cleaned_data['email'],
-#                password=random_pass)
-#            user.is_active = True
-#            user.save()
-#            send_mail(
-#                'Registro en telovendo.cl',
-#                'Bienvenido a Telovendo, su contraseña es: ' + random_pass,
-#                settings.EMAIL_HOST_USER,
-#                (user.email,),
-#                fail_silently=False,
-#            )
-#            print(
-#                f'El username {user.username}, cuyo correo es {user.email} tiene la contraseña: {random_pass}')
-#            return redirect('login')
-#        else:
-#            print(formulario.errors)
-#            context = {'formulario': formulario}
-#            return render(request, 'registration/signup.html', context)
-#
+class Registro(View):
+   def get(self, request):
+       formulario = SignUpForm(request.POST)
+       context = {'formulario': formulario}
+       template_name = "registration/signup.html"
+       return render(request, template_name, context)
+
+   def post(self, request):
+       formulario = SignUpForm(request.POST)
+       if formulario.is_valid():
+           random_pass = User.objects.make_random_password(
+               length=8, allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789')
+           user = User.objects.create_user(
+               username=formulario.cleaned_data['username'],
+               email=formulario.cleaned_data['email'],
+               password=random_pass)
+           user.is_active = True
+           user.save()
+           send_mail(
+               'Registro en telovendo.cl',
+               'Bienvenido a Telovendo, su contraseña es: ' + random_pass,
+               settings.EMAIL_HOST_USER,
+               (user.email,),
+               fail_silently=False,
+           )
+           print(
+               f'El username {user.username}, cuyo correo es {user.email} tiene la contraseña: {random_pass}')
+           return redirect('login')
+       else:
+           print(formulario.errors)
+           context = {'formulario': formulario}
+           return render(request, 'registration/signup.html', context)
